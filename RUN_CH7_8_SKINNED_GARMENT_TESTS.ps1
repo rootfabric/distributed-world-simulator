29d278d5c47a6a44d421b635d307a5ca03738c41
root <yglamazdin@gmail.com>
2026-08-08 20:36:45 +1000

    test(ch7.8): include rigid-first body hiding order gate

--- RUN_CH7_8_SKINNED_GARMENT_TESTS.ps1
+++ RUN_CH7_8_SKINNED_GARMENT_TESTS.ps1
@@ -95,6 +95,7 @@ function Invoke-Godot-Test([string]$Name, [string]$ScriptPath) {
 $Godot = Resolve-Godot $GodotPath
 Invoke-Godot-Test "ch7_8_skinned_garment_pose_bridge" "res://tests/characters/test_ch7_8_skinned_garment_pose_bridge.gd"
 Invoke-Godot-Test "ch7_8_body_region_replacement" "res://tests/characters/test_ch7_8_body_region_replacement.gd"
+Invoke-Godot-Test "ch7_8_quaternius_body_hiding_order" "res://tests/characters/test_ch7_8_quaternius_body_hiding_order.gd"
 Invoke-Godot-Test "ch7_8_quaternius_skinned_garment_lab" "res://tests/characters/test_ch7_8_quaternius_skinned_garment_lab.gd"
 
 Write-Host ""
