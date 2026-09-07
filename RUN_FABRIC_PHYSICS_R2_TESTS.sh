02313b42903c3daed69a7a1f316df818a1d639bd
root <yglamazdin@gmail.com>
2026-09-07 18:45:42 +1000

    test(fabric): run PHYSICS-R2 metamorphic acceptance

--- RUN_FABRIC_PHYSICS_R2_TESTS.sh
+++ RUN_FABRIC_PHYSICS_R2_TESTS.sh
@@ -9,3 +9,7 @@ bash "$ROOT/scripts/research/fabric_bake0/run_adaptive_fidelity_suite.sh" \
 bash "$ROOT/scripts/research/fabric_bake0/run_adaptive_fidelity_suite.sh" \
   res://tests/research/fabric1/fabric_physics_r2_contract_acceptance.gd \
   'FABRIC-PHYSICS-R2-CONTRACT: PASS'
+
+bash "$ROOT/scripts/research/fabric_bake0/run_adaptive_fidelity_suite.sh" \
+  res://tests/research/fabric1/fabric_physics_r2_metamorphic_acceptance.gd \
+  'FABRIC-PHYSICS-R2-METAMORPHIC: PASS'
