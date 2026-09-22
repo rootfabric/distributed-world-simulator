2a1fa67ababac1023e81a6e852d263e4760b15e9
root <yglamazdin@gmail.com>
2026-09-23 01:17:38 +1000

    FABRIC R5.2 T10: account hierarchical leaf complexity

--- scripts/research/fabric_bake0/r5_t10_laser_cannon_compiler_v1.gd
+++ scripts/research/fabric_bake0/r5_t10_laser_cannon_compiler_v1.gd
@@ -57,11 +57,16 @@ static func compile(
 	if interface.is_empty():return U.failure("LASER_CANNON_INTERFACE_CREATE_FAILED")
 	var surface_t:=float(graph.optics_profile.surface_transmission_ratio)
 	var total_t:=pow(surface_t,4.0)
-	var source_components:=5
+	var source_components:=(
+		int(graph.power_stage_capsule.source_component_count)
+		+int(graph.laser_emitter_capsule.source_component_count)
+		+int(graph.cooling_capsule.source_component_count)
+		+2
+	)
 	var source_operations:=(
-		int(graph.power_stage_capsule.executable_operation_count)
-		+int(graph.laser_emitter_capsule.executable_operation_count)
-		+int(graph.cooling_capsule.executable_operation_count)
+		int(graph.power_stage_capsule.full_operation_count)
+		+int(graph.laser_emitter_capsule.full_operation_count)
+		+int(graph.cooling_capsule.full_operation_count)
 		+8
 	)
 	var compiled_operations:=24
